089af50acb0c3a211d5a1d998341cda88c0afcb8
Kaian Samuel <kaiansamuelster@gmail.com>
2023-05-14 20:18:17 -0300

    Lista e Exercicios

--- Jsx/src/index.jsx
+++ Jsx/src/index.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Exercicio from './Exercicio.jsx'
+import Arrays from './Arrays.jsx'
+import ExArrays from './ExArrays.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div>
     <App />
     <Exercicio />
+    <Arrays />
+    <ExArrays />
   </div>
 
 )
